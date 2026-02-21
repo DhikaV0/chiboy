d4ba757f384182be215c919b33e12829cd73495a
DhikaV0 <167159222+DhikaV0@users.noreply.github.com>
2026-02-21 11:04:43 +0700

    add cache remover

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+export const revalidate = 0;
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://chiboy.vercel.app";
 
